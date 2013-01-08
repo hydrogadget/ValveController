d99e7d0fdb0d2eb045341b185343860bf8ac4a06
Brian Munroe <brian.e.munroe@gmail.com>
2013-01-07 22:47:17 -0800

    core controller logic written...looks right on paper! :-)

--- controller.py
+++ controller.py
@@ -1,7 +1,13 @@
 import sys, time
 from daemon import Daemon
+import requests
 
 TASK_SERVICE_URL = 'http://localhost:5000/next/priority'
+CHECK_FOR_NEW_EVENTS_INTERVAL = 5
+NULL_EVENT = {'valve':0,'duration':0,'start_time':0,'command':0}
+
+STOP_COMMAND = 1
+MANUAL_RUN_COMMAND = 2
 
 __all__ = ['ValveController']
 
@@ -11,7 +17,10 @@ def _get_next_priority_event():
 def _get_next_event():
     pass
 
-def _do_event():
+def _open_valve():
+    pass
+
+def _close_valve():
     pass
 
 class ValveController(Daemon):
@@ -19,5 +28,31 @@ class ValveController(Daemon):
         pass
 
     def run(self):
+
+        next_event = None
+
         while True:
+
+            if (next_event == None):
+                current_event = _get_next_event()
+
+            if (current_event != NULL_EVENT):
+                run_time = int(x['duration']) * 60
+                valve_id = _open_valve(current_event['valve'])
+
+                while run_time > 0:
+                    run_time = run_time - 5
+                    time.sleep(5)
+                    priority_event = _get_next_priority_event()
+
+                    # Continue as normal
+                    if (priority_event == NULL_EVENT):
+                        next_event = None
+
+                    # Need to interrupt the current event
+                    if (priority_event != NULL_EVENT):
+                        valve_id = _close_valve(current_event)
+                        current_event = priority_event
+                        next_event == priority_event
+                        break
 
